529d41c87b3c301c4177ef73d95a462daba0f544
Damien Broggini <damienbroggini@hotmail.fr>
2020-06-12 13:30:48 +0200

    modif calculatrice finie

--- calculatrice/code.js
+++ calculatrice/code.js
@@ -1,24 +1,4 @@
-var addition = 1;
-var multiplication = 2;
-var soustraction = 3;
-var division = 4;
-var choix = [soustraction, multiplication, soustraction, division];
-var premierNombre;
-var deuxiemeNombre;
-var operationAddition; //= (premierNombre + deuxiemeNombre);
-var operationMultiplication; //= (premierNombre, deuxiemeNombre);
-var operationSoustraction; //= (premierNombre, deuxiemeNombre);
-var operationDivision; //= (premierNombre, deuxiemeNombre);
 
-do{
-    var choix = Number(prompt("Choisissez une opération : " + "\n 1-addition"+ "\n 2-multiplication"+ 
-        "\n 3-soustraction"+ "\n 4-division"));
-}while(choix != 1 && choix != 2 && choix != 3 & choix != 4)
-
-do{
-    premierNombre = Number(prompt("Ecrivez le premier chiffre : "));
-    deuxiemeNombre = Number(prompt("Ecrivez le deuxieme chiffre : "));
-}while( isNaN(premierNombre) || isNaN(deuxiemeNombre) )
 
 function operationAddition (nombreUn, nombreDeux){
     return nombreUn + nombreDeux;
@@ -33,31 +13,51 @@ function operationSoustraction (nombreUn, nombreDeux){
 }
 
 function operationDivision (nombreUn, nombreDeux){
+    if(nombreDeux == 0){
+        throw new Error ("Impossible de diviser par 0");
+    }
     return nombreUn / nombreDeux;
 }
 
-try {
-    switch (choix) {
-        case 1:
-            var resultat = (operationAddition(premierNombre, deuxiemeNombre));
-            break;
-        case 2:
-            var resultat = (operationMultiplication(premierNombre, deuxiemeNombre));
-            break;
-        case 3:
-            var resultat = (operationSoustraction(premierNombre, deuxiemeNombre));
-            break;
-        case 4:
-            var resultat = (operationDivision(premierNombre, deuxiemeNombre));
-            break;
-        default:
-            throw new error ("vous ne pouvez pas diviser par 0");
+let restart = false ;
+do{
+
+    do{
+        var choix = Number(prompt("Choisissez une opération : " + "\n 1-addition"+ "\n 2-multiplication"+ 
+            "\n 3-soustraction"+ "\n 4-division\n"));
+    }while(choix != 1 && choix != 2 && choix != 3 & choix != 4)
+
+    do{
+        var premierNombre = Number(prompt("Ecrivez le premier chiffre : "));
+        var deuxiemeNombre = Number(prompt("Ecrivez le deuxieme chiffre : "));
+    }while( isNaN(premierNombre) || isNaN(deuxiemeNombre) )
+
+
+
+    try {
+        switch (choix) {
+            case 1:
+                var resultat = (operationAddition(premierNombre, deuxiemeNombre));
+                break;
+            case 2:
+                var resultat = (operationMultiplication(premierNombre, deuxiemeNombre));
+                break;
+            case 3:
+                var resultat = (operationSoustraction(premierNombre, deuxiemeNombre));
+                break;
+            case 4:
+                var resultat = (operationDivision(premierNombre, deuxiemeNombre));
+                break;
+            default:
+                throw new Error ("Une erreur est survenue");
+        }
+        alert("le résultat de l'opération est " + resultat);
+    
+    } 
+    catch (error) {
+        alert(error);
     }
-    alert("le résultat de l'opération est " + resultat);
-} 
-catch (error) {
-    alert(error);
-}
-finally{
-    alert('fin du programme');
-}
+    restart = confirm('Souhaitez vous effecter un nouveau calcul?');
+
+}while(restart)
+
